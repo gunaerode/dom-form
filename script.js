6b014fdc21d8ae1497bbe49d7a920d0aba6912db
gunaerode <guna.learn2016@gmail.com>
2021-07-14 01:47:33 +0530

    fix(main): Bug fixes(:bug:) :fire: :snowflake:

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ function createElement(
   Array.isArray(attribute)
     ? newElement.setAttribute(attribute[0], attribute[1])
     : null;
-  content ? (newElement.innerText = content) : null;
+  content ? (newElement.innerHTML = content) : null;
+  let spanExcludeArray = [
+    "Male",
+    "Female",
+    "Sweet",
+    "Spicy",
+    "Salty",
+    "Bitter",
+    "Sour",
+  ];
+  element === "label"
+    ? !spanExcludeArray.includes(content)
+      ? (newElement.innerHTML += " <span>*</span>")
+      : null
+    : null;
   return newElement;
 }
 
@@ -20,7 +34,7 @@ function createFormInputElement(
   attributeValue
 ) {
   let divElement = createElement("div", divClass);
-  let labelElement = createElement("label", false, labelContent, [
+  let labelElement = createElement("label", "label", labelContent, [
     "for",
     attributeValue,
   ]);
@@ -39,7 +53,8 @@ function createFormRadioElement(
   inputType,
   labelContent = false,
   attributeValue,
-  type
+  type,
+  checked
 ) {
   let divElement = createElement("div", "form-check-inline");
   let labelElement = createElement("label", false, labelContent, [
@@ -53,21 +68,30 @@ function createFormRadioElement(
   inputElement.setAttribute("type", type);
   inputElement.setAttribute("id", attributeValue);
   inputElement.setAttribute("value", attributeValue);
+  checked ? inputElement.setAttribute("checked", true) : null;
   form.append(divElement);
   divElement.append(labelElement, inputElement);
 }
 
+function capitalize(string) {
+  return string[0]
+    ? string[0].toUpperCase() + string.substr(1, string.length)
+    : string;
+}
+
 function createCheckBoxElement(foodArray) {
   for (let value of foodArray) {
+    let valueCaps = capitalize(value);
     let divElement = createElement("div", "form-check-inline");
-    let labelElement = createElement("label", false, value, ["for", value]);
+    let labelElement = createElement("label", false, valueCaps, ["for", value]);
     let inputElement = createElement("input", "form-check-input", false, [
       "name",
-      "food[]",
+      "food",
     ]);
     inputElement.setAttribute("type", "checkbox");
     inputElement.setAttribute("id", value);
     inputElement.setAttribute("value", value);
+    // value === "sweet" ? inputElement.setAttribute("required", true) : null;
     form.append(divElement);
     divElement.append(labelElement, inputElement);
   }
@@ -90,22 +114,31 @@ row.append(colOne, colTwo);
 // =========== Form Elements ==============
 let form,
   title,
+  required,
   firstName,
   lastName,
   address,
   pincode,
   clearElementRadio,
+  genderLabel,
   genderMale,
   genderFemale,
   clearElementCheck,
+  foodLabel,
   choiceOfFood = ["sweet", "spicy", "bitter", "salty", "sour"],
+  foodError,
   State,
   Country,
   submitButton;
 
 form = createElement("form", "formElement", false, ["method", "POST"]);
-title = createElement("h2", "title text-center", "Fill the form");
-colOne.append(title, form);
+title = createElement("div", "title text-center", "Personal Details");
+required = createElement(
+  "div",
+  "text-center",
+  "<span>*</span> These fields are required"
+);
+colOne.append(title, required, form);
 
 firstName = createFormInputElement(
   "input",
@@ -128,11 +161,23 @@ address = createFormInputElement(
 pincode = createFormInputElement("input", "form-group", "Pincode", "pincode");
 clearElementRadio = createElement("div", "clearfix");
 form.append(clearElementRadio);
-genderMale = createFormRadioElement("input", "Male", "male", "radio");
-genderFemale = createFormRadioElement("input", "Female", "female", "radio");
+genderLabel = createElement("label", "label", "Select gender");
+form.append(genderLabel);
+genderMale = createFormRadioElement("input", "Male", "male", "radio", true);
+genderFemale = createFormRadioElement(
+  "input",
+  "Female",
+  "female",
+  "radio",
+  false
+);
 clearElementCheck = createElement("div", "clearfix");
 form.append(clearElementCheck);
+foodLabel = createElement("label", "label", "Choice of food");
+form.append(foodLabel);
 choiceOfFood = createCheckBoxElement(choiceOfFood);
+foodError = createElement("div", "foodError");
+form.append(foodError);
 state = createFormInputElement("input", "form-group", "State", "state");
 country = createFormInputElement("input", "form-group", "Country", "country");
 submitButton = createElement(
@@ -148,7 +193,23 @@ let formDom = document.querySelector(".formElement");
 formDom.addEventListener("submit", function (event) {
   event.preventDefault();
   const formData = new FormData(this);
-  let table, thead, tbody, tr1, tr2, th1, th2;
+  let table,
+    thead,
+    tbody,
+    food = [];
+  let tempTable = document.querySelector(".table");
+  tempTable ? tempTable.remove() : null;
+  let foodSelectCount = document.querySelectorAll(
+    "[name='food']:checked"
+  ).length;
+  let foodError = document.querySelector(".foodError");
+  if (foodSelectCount < 2) {
+    foodError.innerText = "Select atleast 2 foods";
+    tempTable ? tempTable.remove() : null;
+    return false;
+  } else {
+    foodError.innerText = " ";
+  }
 
   table = createElement("table", "table table-dark");
   colTwo.append(table);
@@ -165,11 +226,22 @@ formDom.addEventListener("submit", function (event) {
 
   for (const arrayValue of formData) {
     let [key, value] = arrayValue;
-    tr2 = createElement("tr");
-    tbody.append(tr2);
-    th2 = createElement("th", false, key);
-    td3 = createElement("td", false, value);
-    tr2.append(th2, td3);
+    if (key === "food") {
+      food.push(capitalize(value));
+      continue;
+    } else {
+      key === "state" ? createRow(tbody, "food", food.join()) : null;
+    }
+    createRow(tbody, key, value);
   }
   form.reset();
 });
+
+function createRow(tbody, key, value) {
+  let tr1, th1, td1;
+  tr1 = createElement("tr");
+  tbody.append(tr1);
+  th1 = createElement("th", false, capitalize(key));
+  td1 = createElement("td", false, capitalize(value));
+  tr1.append(th1, td1);
+}
